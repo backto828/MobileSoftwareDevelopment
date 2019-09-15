5deb657c311b0dffecd0d194497f33c80b01e84d
backto828 <411532139@qq.com>
2019-09-15 20:44:35 +0800

    V3 Button events finished

--- app/src/main/java/com/example/textview001/ButtonMainActivity.java
+++ app/src/main/java/com/example/textview001/ButtonMainActivity.java
@@ -3,12 +3,37 @@ package com.example.textview001;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ButtonMainActivity extends AppCompatActivity {
 
+    private Button buttonHelloEn, buttonHelloCz;
+    private TextView textViewHelloWorld;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button_main);
+
+        buttonHelloEn=this.findViewById(R.id.buttonHelloEn);
+        buttonHelloCz=this.findViewById(R.id.buttonHelloCz);
+        textViewHelloWorld=this.findViewById(R.id.textViewHelloWorld);
+
+        buttonHelloCz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textViewHelloWorld.setText(buttonHelloCz.getText());
+            }
+        });
+
+        buttonHelloEn.setOnClickListener(new ButtonCzOnClick());
+    }
+
+    private class ButtonCzOnClick implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            textViewHelloWorld.setText(((Button)view).getText());
+        }
     }
 }
